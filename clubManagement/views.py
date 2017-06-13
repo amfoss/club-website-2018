9ba7c6ecc5734f067e64f61b9a2c05e8547692a9
Chirath R <chirath.02@gmail.com>
2017-06-13 22:08:31 +0530

    Yearly graph

--- clubManagement/views.py
+++ clubManagement/views.py
@@ -5,7 +5,7 @@ from datetime import date
 
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
-from django.views.generic import View
+from django.views.generic import View, YearArchiveView
 from clubManagement.models import Attendance
 from registration.models import UserInfo
 
@@ -28,7 +28,8 @@ class AttendanceAddView(View):
             try:
                 attendance = Attendance.objects.get(user=user_info.user, date=d)
             except Attendance.DoesNotExist:
-                attendance = Attendance(user=user_info.user, added_by=User.objects.get(username=self.request.user.username), date=d)
+                attendance = Attendance(user=user_info.user,
+                                        added_by=User.objects.get(username=self.request.user.username), date=d)
                 attendance.save()
             attendance_list.append(attendance)
         return {'attendance_list': attendance_list}
@@ -49,3 +50,21 @@ class AttendanceAddView(View):
                 att.attendance = True
                 att.save()
         return redirect('add_attendance', **kwargs)
+
+
+class YearAttendanceReportView(View):
+    template_name = 'clubManagement/attendance_yearly.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        if 'user_id' in kwargs:
+            user = User.objects.get(id=int(kwargs.get('user_id')))
+            att = user.attendance_set.filter(date__year=int(kwargs.get('year')))
+            month_att = []
+            month = ["January", "February", "March", "April", "May", "June", "July", "August", "September",
+                     "October", "November", "December"]
+            month_num = range(12)
+            for i in month_num:
+                month_att.append(len(att.filter(date__month=(i + 1))))
+            context = {'user': user, 'month_att': month_att, 'month': month, 'month_num': month_num, 'year': kwargs.get('year')}
+        return render(request, self.template_name, context)
