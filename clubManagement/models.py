6b3d09777614d91518bc25bb9e016b3cb3a8a20a
Chirath R <chirath.02@gmail.com>
2017-06-13 14:03:23 +0530

    Added project and alubManagement models

--- clubManagement/models.py
+++ clubManagement/models.py
@@ -1,6 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Attendance(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="added_by")
+    date = models.DateTimeField()
+    modified_date = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.user.username + " " + str(self.date)
+
+
+class Responsibility(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    responsibility = models.CharField(max_length=300)
+    more_info = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username + " " + self.responsibility
+
+    def get_absolute_url(self):
+        return " "
+
+
+class StatusReport(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    content = models.TextField()
+    img = models.ImageField(upload_to='status_img/', blank=True)
+    # project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username + " " + self.content[:20] + ".."
+
+    def get_absolute_url(self):
+        return " "
--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=300)
+    description = models.TextField(blank=True)
+    date_completed = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.created_by.username + ' ' + self.title
+
+    def get_absolute_url(self):
+        return " "
+
+
+class ProjectImages(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='project/')
+
+    def __str__(self):
+        return self.project
