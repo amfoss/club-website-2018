e9f4a00a784ca43e9ef9f9dbf706511db296ea0e
Chirath R <chirath.02@gmail.com>
2017-06-15 22:05:04 +0530

    project and event to admin

--- projects/admin.py
+++ projects/admin.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Project)
+admin.site.register(ProjectMembers)
+admin.site.register(ProjectImages)
