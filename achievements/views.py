ee9247e13915a14326206fd5ee9d3b60a693daa9
Chirath R <chirath.02@gmail.com>
2017-06-18 15:45:16 +0530

    Fixed footer and navbar height

--- achievements/views.py
+++ achievements/views.py
@@ -2,5 +2,52 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import *
+from django.contrib.auth.models import User
+
+
+class ArticleListView(ListView):
+    model = Article
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ['user', 'title', 'description', 'magazine', 'publication_date']
+
+    def form_valid(self, form):
+        form.cleaned_data['user'] = self.request.user
+        return super(ArticleCreateView, self).form_valid(form)
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+
+    def get(self, request, *args, **kwargs):
+
+        return super(ArticleUpdateView, self).get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        form.cleaned_data['user'] = self.request.user
+        return super(ArticleUpdateView, self).form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+
+        return super(ArticleUpdateView, self).post(request, *args, **kwargs)
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+
+    def get(self, request, *args, **kwargs):
+
+        return super(ArticleDeleteView, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+
+        return super(ArticleDeleteView, self).post(request, *args, **kwargs)
