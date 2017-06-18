2b304f9611327ac9dbb1352ded63b3bf29de65ee
Chirath R <chirath.02@gmail.com>
2017-06-18 18:55:03 +0530

    articles permissions

--- achievements/views.py
+++ achievements/views.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import *
@@ -11,6 +14,12 @@ class ArticleListView(ListView):
 
 class ArticleDetailView(DetailView):
     model = Article
+
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        if self.request.user.is_superuser or self.request.user == self.get_object().user:
+            context['edit_permission'] = True
+        return context
 
 
 class ArticleCreateView(CreateView):
@@ -27,7 +36,8 @@ class ArticleUpdateView(UpdateView):
     fields = ['title', 'area', 'description', 'magazine', 'publication_date']
 
     def get(self, request, *args, **kwargs):
-
+        if not (request.user.is_superuser or request.user == self.get_object().created_by):
+            redirect('permission_denied')
         return super(ArticleUpdateView, self).get(request, *args, **kwargs)
 
     def form_valid(self, form):
@@ -35,17 +45,21 @@ class ArticleUpdateView(UpdateView):
         return super(ArticleUpdateView, self).form_valid(form)
 
     def post(self, request, *args, **kwargs):
-
+        if not (request.user.is_superuser or request.user == self.get_object().created_by):
+            redirect('permission_denied')
         return super(ArticleUpdateView, self).post(request, *args, **kwargs)
 
 
 class ArticleDeleteView(DeleteView):
     model = Article
+    success_url = reverse_lazy('article')
 
     def get(self, request, *args, **kwargs):
-
+        if not (request.user.is_superuser or request.user == self.get_object().created_by):
+            redirect('permission_denied')
         return super(ArticleDeleteView, self).get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-
+        if not (request.user.is_superuser or request.user == self.get_object().created_by):
+            redirect('permission_denied')
         return super(ArticleDeleteView, self).post(request, *args, **kwargs)
