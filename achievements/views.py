bef71336f6539a77a173503b548b0f5417423625
Chirath R <chirath.02@gmail.com>
2017-06-19 09:46:12 +0530

    added speaker view

--- achievements/views.py
+++ achievements/views.py
@@ -36,7 +36,7 @@ class ArticleUpdateView(UpdateView):
     fields = ['title', 'area', 'description', 'magazine', 'date']
 
     def get(self, request, *args, **kwargs):
-        if not (request.user.is_superuser or request.user == self.get_object().created_by):
+        if not (request.user.is_superuser or request.user == self.get_object().user):
             redirect('permission_denied')
         return super(ArticleUpdateView, self).get(request, *args, **kwargs)
 
@@ -45,7 +45,7 @@ class ArticleUpdateView(UpdateView):
         return super(ArticleUpdateView, self).form_valid(form)
 
     def post(self, request, *args, **kwargs):
-        if not (request.user.is_superuser or request.user == self.get_object().created_by):
+        if not (request.user.is_superuser or request.user == self.get_object().user):
             redirect('permission_denied')
         return super(ArticleUpdateView, self).post(request, *args, **kwargs)
 
@@ -56,14 +56,15 @@ class ArticleDeleteView(DeleteView):
     success_url = reverse_lazy('article')
 
     def get(self, request, *args, **kwargs):
-        if not (request.user.is_superuser or request.user == self.get_object().created_by):
+        if not (request.user.is_superuser or request.user == self.get_object().user):
             redirect('permission_denied')
         return super(ArticleDeleteView, self).get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        if not (request.user.is_superuser or request.user == self.get_object().created_by):
+        if not (request.user.is_superuser or request.user == self.get_object().user):
             redirect('permission_denied')
         return super(ArticleDeleteView, self).post(request, *args, **kwargs)
+
 
 # Contribution views
 
@@ -96,7 +97,7 @@ class ContributionUpdateView(UpdateView):
     fields = ['contribution_id', 'title', 'organisation', 'url', 'description', 'date']
 
     def get(self, request, *args, **kwargs):
-        if not (request.user.is_superuser or request.user == self.get_object().created_by):
+        if not (request.user.is_superuser or request.user == self.get_object().user):
             redirect('permission_denied')
         return super(ContributionUpdateView, self).get(request, *args, **kwargs)
 
@@ -105,7 +106,7 @@ class ContributionUpdateView(UpdateView):
         return super(ContributionUpdateView, self).form_valid(form)
 
     def post(self, request, *args, **kwargs):
-        if not (request.user.is_superuser or request.user == self.get_object().created_by):
+        if not (request.user.is_superuser or request.user == self.get_object().user):
             redirect('permission_denied')
         return super(ContributionUpdateView, self).post(request, *args, **kwargs)
 
@@ -116,14 +117,74 @@ class ContributionDeleteView(DeleteView):
     success_url = reverse_lazy('contribution')
 
     def get(self, request, *args, **kwargs):
-        if not (request.user.is_superuser or request.user == self.get_object().created_by):
+        if not (request.user.is_superuser or request.user == self.get_object().user):
             redirect('permission_denied')
         return super(ContributionDeleteView, self).get(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        if not (request.user.is_superuser or request.user == self.get_object().created_by):
+        if not (request.user.is_superuser or request.user == self.get_object().user):
             redirect('permission_denied')
         return super(ContributionDeleteView, self).post(request, *args, **kwargs)
 
+
 # GSoc Views
 
+# Speaker views
+
+
+class SpeakerListView(ListView):
+    model = Speaker
+
+
+class SpeakerDetailView(DetailView):
+    model = Speaker
+
+    def get_context_data(self, **kwargs):
+        context = super(SpeakerDetailView, self).get_context_data(**kwargs)
+        if self.request.user.is_superuser or self.request.user == self.get_object().user:
+            context['edit_permission'] = True
+        return context
+
+
+class SpeakerCreateView(CreateView):
+    model = Speaker
+    fields = ['title', 'type', 'conference_name', 'location', 'url', 'date', 'description']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(SpeakerCreateView, self).form_valid(form)
+
+
+class SpeakerUpdateView(UpdateView):
+    model = Speaker
+    fields = ['title', 'type', 'conference_name', 'location', 'url', 'date', 'description']
+
+    def get(self, request, *args, **kwargs):
+        if not (request.user.is_superuser or request.user == self.get_object().user):
+            redirect('permission_denied')
+        return super(SpeakerUpdateView, self).get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(SpeakerUpdateView, self).form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+        if not (request.user.is_superuser or request.user == self.get_object().user):
+            redirect('permission_denied')
+        return super(SpeakerUpdateView, self).post(request, *args, **kwargs)
+
+
+class SpeakerDeleteView(DeleteView):
+    model = Speaker
+    template_name = 'achievements/confirm_delete.html'
+    success_url = reverse_lazy('speaker')
+
+    def get(self, request, *args, **kwargs):
+        if not (request.user.is_superuser or request.user == self.get_object().user):
+            redirect('permission_denied')
+        return super(SpeakerDeleteView, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if not (request.user.is_superuser or request.user == self.get_object().user):
+            redirect('permission_denied')
+        return super(SpeakerDeleteView, self).post(request, *args, **kwargs)
