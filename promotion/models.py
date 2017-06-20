6bf86a788f3e8bd2b61e8815b7315e1b4015b450
Chirath R <chirath.02@gmail.com>
2017-06-21 00:10:14 +0530

    new model in promotions

--- promotion/models.py
+++ promotion/models.py
@@ -3,5 +3,32 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+batch_choices = (
+    ('1st year', '1st year'),
+    ('2nd year', 'wnd year'),
+    ('3rd year', '3rd year'),
+    ('4th year', '4t year'),
+)
+
+
+class JoinApplication(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    batch = models.CharField(max_length=100, choices=batch_choices)
+    motivation = models.TextField()
+    cs_background = models.TextField(blank=True)
+    previous_work = models.TextField(blank=True)
+    interests = models.TextField()
+
+    date = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('join_application_detail', kwargs={'pk', self.id})
+
+    class Meta:
+        ordering = ('date',)
