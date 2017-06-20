5b41aa0b2b4a0a5fe7323248b594e957a5c4b69f
Chirath R <chirath.02@gmail.com>
2017-06-21 00:10:47 +0530

    View in promotions

--- promotion/views.py
+++ promotion/views.py
@@ -3,5 +3,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 
-# Create your views here.
+from promotion.models import JoinApplication
+
+
+class JoinApplicationListView(ListView):
+    model = JoinApplication
+
+
+class JoinApplicationDetailView(DetailView):
+    model = JoinApplication
+
+
+class JoinApplicationCreateView(CreateView):
+    model = JoinApplication
