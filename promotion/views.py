305d6ff44dacf6610c7fcc81dbce882d409ea812
Chirath R <chirath.02@gmail.com>
2017-06-22 10:04:18 +0530

    email for application approval and reection

--- promotion/views.py
+++ promotion/views.py
@@ -2,12 +2,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from smtplib import SMTPException
+
+from django.contrib.auth.models import User
+from django import forms
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.core.mail import send_mail
+
 
 from promotion.forms import JoinApplicationForm
 from promotion.models import JoinApplication
+
+
+approve_mail_content = ',\\n\\nWe are exited to inform that you are selected for the interview.'
+reject_mail_content = ',\\n\\nWe are sorry to inform that you are not selected for the interview. Please try again ' \
+                      'next time.'
 
 
 class JoinApplicationListView(ListView):
@@ -25,15 +36,23 @@ class JoinApplicationDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(JoinApplicationDetailView, self).get_context_data(**kwargs)
-        context['mail_subject'] = 'Congrats! You are selected for interview'
-        context['mail_content'] = 'Hi ' + self.get_object().name + ' We are exited to inform that you are selected ' \
-                                                                   'for the interview.'
+        context['approve_mail_subject'] = 'Congrats! FOSS@Amrita membership application'
+        context['approve_mail_content'] = 'Hi ' + self.get_object().name + approve_mail_content
+        context['reject_mail_subject'] = 'FOSS@Amrita membership application'
+        context['reject_mail_content'] = 'Hi ' + self.get_object().name + reject_mail_content
+        context['mail_error'] = kwargs.get('mail_errors', None)
         return context
 
 
 class JoinApplicationCreateView(CreateView):
     form_class = JoinApplicationForm
     template_name = 'base/form.html'
+
+
+class EmailForm(forms.Form):
+    mail_id = forms.EmailField()
+    mail_subject = forms.CharField()
+    mail_content = forms.CharField()
 
 
 class JoinApplicationUpdateView(UpdateView):
@@ -46,3 +65,24 @@ class JoinApplicationUpdateView(UpdateView):
     def post(self, request, *args, **kwargs):
         if not (request.user.is_superuser or request.user == self.get_object().created_by):
             return redirect('permission_denied')
+
+        form = EmailForm(request.POST)
+
+        if form.is_valid():
+            # mail id of all the admins and the mail id from the form
+            to_address_list = list(User.objects.filter(is_superuser=True).values_list('email', flat=True))
+            to_address_list.append(form.cleaned_data['mail_id'])
+
+            # sent mail, if there are errors in mail, check that too
+            mail_errors = False
+            try:
+                send_mail(form.cleaned_data['mail_subject'], form.cleaned_data['mail_content'],
+                          'amritapurifoss@gmail.com', to_address_list, fail_silently=False)
+            except SMTPException:
+                mail_errors = True
+
+            # render the detail page
+            return redirect('join_detail', self.get_object().id)
+
+        # error in form
+        return redirect('join_detail', self.get_object().id)
