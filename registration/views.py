934e2f2740579ef661ff29c828e6a555c5fab63c
Chirath R <chirath.02@gmail.com>
2017-06-25 02:31:50 +0530

    update profile successful

--- registration/views.py
+++ registration/views.py
@@ -36,3 +36,31 @@ class UserUpdateView(UpdateView):
     form_class = UserForm
     template_name = 'base/form.html'
 
+    def form_valid(self, form):
+        response = super(UserUpdateView, self).form_valid(form)
+        if form.is_valid():
+            user = form.instance.user
+            user.first_name = form.cleaned_data['first_name']
+            user.last_name = form.cleaned_data['last_name']
+            user.email = form.cleaned_data['email']
+            user.save()
+        return response
+
+    def get_context_data(self, **kwargs):
+        context = super(UserUpdateView, self).get_context_data(**kwargs)
+        user = context['form'].instance.user
+        context['form'].fields['first_name'].initial = user.first_name
+        context['form'].fields['last_name'].initial = user.last_name
+        context['form'].fields['email'].initial = user.email
+        context['heading'] = 'Update profile'
+        return context
+
+    def get(self, request, *args, **kwargs):
+        if request.user != self.get_object().user:
+            return redirect('permission_denied')
+        return super(UserUpdateView, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        if request.user != self.get_object().user:
+            return redirect('permission_denied')
+        return super(UserUpdateView, self).post(request, *args, **kwargs)
