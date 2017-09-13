8c86bc89e3bf87d7d67240cd21e4ad6cbc1abd7c
Chirath R <chirath.02@gmail.com>
2017-09-13 18:35:41 +0530

    Workshop model

--- workshop/models.py
+++ workshop/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Workshop(models.Model):
+    name = models.CharField(max_length=500)
+    user = models.ForeignKey(User)
+    description = models.TextField()
+    level = models.CharField(blank=True)
+    poster = models.ImageField(upload_to='workshop/poster/', null=True, blank=True)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+
+    def __str__(self):
+        return self.name
