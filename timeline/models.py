994af7893646ea88ee0c371bddd6e2ba0877aeb4
Chirath R <chirath.02@gmail.com>
2017-09-15 10:00:39 +0530

    bug fix

--- timeline/models.py
+++ timeline/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class AlumniInfo(models.Model):
     name = models.CharField(max_length=100, blank=True)
     email = models.EmailField(blank=True)
