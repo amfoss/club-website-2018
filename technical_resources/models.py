8d1f09fa89b16a884b18f895c8360e20a7dcff49
Chirath R <chirath.02@gmail.com>
2017-09-18 12:49:13 +0530

    technical_resources model

--- technical_resources/models.py
+++ technical_resources/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='technical_resource/images/')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Links(models.Model):
+    category = models.ForeignKey(Category)
+    name = models.CharField(max_length=300)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.category.name + '-' + self.name
+
+
+class Files(models.Model):
+    category = models.ForeignKey(Category)
+    name = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='technical_resource/files/')
+
+    def __str__(self):
+        return self.category.name + '-' + self.name
