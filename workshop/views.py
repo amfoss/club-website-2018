478ed227bb15461d74b3b39588cf4afb4e7c811b
sreejithsankar55 <sreejithsankar55@gmail.com>
2017-10-16 09:44:37 +0530

    duplicate removed

--- workshop/views.py
+++ workshop/views.py
@@ -149,7 +149,7 @@ class WorkshopRegisterFormView(CreateView):
         mail_content = "Hi " + form.cleaned_data.get('name') + ", \n\n" + \
                        "Great to know that you are interested in '" + workshop.name + "' workshop conducted by" \
                                                                                       "FOSS@Amrita. We got your application and it's being processed. " + \
-                       "We will get back to you once your payment process is is complete.\n\n" \
+                       "We will get back to you once your payment process is complete.\n\n" \
                        "Please pay Rs" + str(workshop.price) + " at FOSS club, ground floor lab after 4:30pm on or " + \
                        "before " + str(workshop.start_date_time.date()) + " or contact us at " + \
                        str(workshop.contact_info) + " during breaks. \n\nNote: Payment has to completed " + \
