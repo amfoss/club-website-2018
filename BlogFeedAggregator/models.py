898bfd5853c1256857856a460b53b2a0cce52915
Aniketh Girish <anikethgireesh@gmail.com>
2017-10-30 18:47:44 +0530

    Model class feed created

--- BlogFeedAggregator/models.py
+++ BlogFeedAggregator/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Feed(models.Model):
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    is_active = models.BooleanField(default=false)
