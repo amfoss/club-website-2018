91e8b6bd0c0fb8c262e4f3558c20fffa767a1414
Aniketh Girish <anikethgireesh@gmail.com>
2017-10-30 21:12:30 +0530

    Admin section created

--- BlogFeedAggregator/admin.py
+++ BlogFeedAggregator/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Feed, Article
 
-# Register your models here.
+admin.site.register(Feed)
+admin.site.register(Article)
