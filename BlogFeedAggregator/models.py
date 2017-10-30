1b3edfc47e07437299aa08c14cb543d5b976fd05
Aniketh Girish <anikethgireesh@gmail.com>
2017-10-30 18:55:31 +0530

    Article class created in the models

--- BlogFeedAggregator/models.py
+++ BlogFeedAggregator/models.py
@@ -7,3 +7,10 @@ class Feed(models.Model):
     title = models.CharField(max_length=200)
     url = models.URLField()
     is_active = models.BooleanField(default=false)
+
+class Article(models.Model):
+    feed = models.ForeignKey(Feed)
+    title = models.CharField(max_length=200)
+    url = models.URLField()
+    description = models.TextField()
+    publication_date = models.DateTimeField()
