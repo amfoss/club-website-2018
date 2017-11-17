bad1ebedca179424ef57807211a61832367a9ec3
Aniketh Girish <anikethgireesh@gmail.com>
2017-11-17 21:19:34 +0530

    First stage refactored admin-panel added

---
+++ Home/static/home/assets/js/admin-panel.js
@@ -0,0 +1,55 @@
+var menu = document.querySelector('.nav__list');
+var burger = document.querySelector('.burger');
+var doc = $(document);
+var l = $('.scrolly');
+var panel = $('.panel');
+var vh = $(window).height();
+
+var openMenu = function() {
+  burger.classList.toggle('burger--active');
+  menu.classList.toggle('nav__list--active');
+};
+
+// reveal content of first panel by default
+panel.eq(0).find('.panel__content').addClass('panel__content--active');
+
+var scrollFx = function() {
+  var ds = doc.scrollTop();
+  var of = vh / 4;
+  
+  // if the panel is in the viewport, reveal the content, if not, hide it.
+  for (var i = 0; i < panel.length; i++) {
+    if (panel.eq(i).offset().top < ds+of) {
+     panel
+       .eq(i)
+       .find('.panel__content')
+       .addClass('panel__content--active');
+    } else {
+      panel
+        .eq(i)
+        .find('.panel__content')
+        .removeClass('panel__content--active')
+    }
+  }
+};
+
+var scrolly = function(e) {
+  e.preventDefault();
+  var target = this.hash;
+  var $target = $(target);
+
+  $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+  }, 300, 'swing', function () {
+      window.location.hash = target;
+  });
+}
+
+var init = function() {
+  burger.addEventListener('click', openMenu, false);
+  window.addEventListener('scroll', scrollFx, false);
+  window.addEventListener('load', scrollFx, false);
+  $('a[href^="#"]').on('click',scrolly);
+};
+
+doc.on('ready', init);
