aaaf2286ec2faa1f783f0b5395c82c4465abdcab
navisk13 <navisk13@gmail.com>
2017-11-28 19:57:16 +0530

    timeline

--- fosswebsite/urls.py
+++ fosswebsite/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     url(r'^accounts/', include('registration.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),#this file is invisible
     url(r'^club/', include('clubManagement.urls')),
-    url(r'^foss/', include('promotion.urls')),
+    url(r'^foss/', include('admissions.urls')),
     url(r'^achievements/', include('achievements.urls')),
     url(r'^documents/', include('documents.urls')),
     url(r'^project/', include('projects.urls')),
