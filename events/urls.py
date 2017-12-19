b5b3aa536d928b134cbd2eaa9a869506340e9a69
navisk13 <navisk13@gmail.com>
2017-12-19 12:26:51 +0530

    finished events app

--- events/urls.py
+++ events/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls import url
 
 from events.forms import EventCreateForm
-from events.views import EventCreateView, EventUpdateView, EventImageCreateView, EventImageUpdateView, EventDeleteView, \
-    EventImageDeleteView, EventDetailView
+from events.views import EventCreateView, EventUpdateView, EventImageCreateView,  EventDeleteView, \
+    EventImageDeleteView, EventDetailView, EventListView, EventImageListView
 
 urlpatterns = [
+    url(r'^$', EventListView.as_view(), name='event'),
+    url(r'^(?P<pk>[0-9]+)$', EventDetailView.as_view(), name = "event_detail"),
     url(r'^create/', EventCreateView.as_view(), name = "Event_add"),
-    url(r'^edit/(?P<pk>[0-9]+)', EventUpdateView.as_view(), name = "Event_edit"),
-    url(r'^addimage/', EventImageCreateView.as_view(), name = "eventimage_add"),
-    url(r'^event/(?P<pk>[0-9]+)', EventDetailView.as_view(), name = "event_detail"),
-    url(r'^editimage/(?P<pk>[0-9]+)', EventImageUpdateView.as_view(), name = "eventimage_edit"),
-    url(r'^deleteimage/(?P<pk>[0-9]+)/', EventDeleteView.as_view(), name = "eventimage_delete"),
-    url(r'^delete/(?P<pk>[0-9]+)', EventImageDeleteView.as_view(), name = "event_delete"),
+    url(r'^update/(?P<pk>[0-9]+)', EventUpdateView.as_view(), name = "Event_edit"),
+    url(r'^delete/(?P<pk>[0-9]+)', EventDeleteView.as_view(), name = "event_delete"),
+    url(r'^(?P<pk>[0-9]+)/images/$', EventImageListView.as_view(), name='eventimage_list'),
+    url(r'^(?P<pk>[0-9]+)/addimage/$', EventImageCreateView.as_view(), name = "eventimage_add"),
+    url(r'^eventimage/(?P<pk>[0-9]+)/delete/$', EventImageDeleteView.as_view(), name = "eventimage_delete"),
+
 
 
 ]
