9b182fb5f4c39245f13b0f1bc29f148dd219e5ce
Aniketh Girish <anikethgireesh@gmail.com>
2017-12-20 16:27:43 +0530

    Overrider fuctions for the model classes in the registration apps

--- registration/models.py
+++ registration/models.py
@@ -48,3 +48,9 @@ class WorkExperience(models.Model):
     position = models.CharField(max_length=100, blank=True)
     company = models.CharField(max_length=100, blank=True)
     work_description = models.TextField(max_length=300, blank=True)
+
+    def __str__(self):
+        return self.workExp.user.username
+
+    def get_absolute_url(self):
+        return reverse('profile', kwargs={'pk': self.id})
