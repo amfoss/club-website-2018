4553f6f21c0f7988f342ff2e4a16573244755411
rahulk98 <rahul.krishnan27@gmail.com>
2018-01-11 18:10:54 +0530

    events app

---
+++ events/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# created by Chirath R, chirath.02@gmail.com
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Event)
