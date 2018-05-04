9e5583ab08bb40d0cedd4931a2fde59b2040d554
Chirath R <chirath.02@gmail.com>
2018-05-04 11:49:36 +0530

    fix error when no ssid is found in db

--- attendance/management/commands/update_ssid.py
+++ attendance/management/commands/update_ssid.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     help = 'Generates a new ssid(need to run as a cron job everyday)'
 
     def handle(self, *args, **options):
-        ssid_name = SSIDName.objects.first()
+        ssid_name, created = SSIDName.objects.get_or_create(id=1)
         ssid_name.generate_random_name()
         ssid_name.save()
         print(ssid_name.name)
