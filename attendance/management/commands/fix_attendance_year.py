4d946656d4909db1891bc3bdf2aac5982b54c008
Chirath R <chirath.02@gmail.com>
2018-07-26 22:06:44 +0530

    Script to update year in attendance

--- attendance/management/commands/fix_attendance_year.py
+++ attendance/management/commands/fix_attendance_year.py
@@ -7,6 +7,7 @@ class Command(BaseCommand):
 
     @staticmethod
     def get_year_string(text):
+        text = text.strip().lower()
         if text == "1st year":
             return "2017"
         elif text == "2nd year":
@@ -17,19 +18,19 @@ class Command(BaseCommand):
             return "2014"
         else:
             print("Error, wrong date", text)
-            exit()
 
     def handle(self, *args, **kwargs):
         attendance_list = DailyAttendance.objects.all()
+
         for attendance_item in attendance_list:
             attendance = attendance_item.attendance
 
-            if str(attendance_item.date)== "2018-07-24":
-                exit()
+            # if str(attendance_item.date)== "2018-07-24":
+            #     exit()
 
             print(attendance_item.date, attendance_item.attendance.keys())
 
-            batch_list = attendance.keys()
+            batch_list = list(attendance.keys())
 
             for batch in batch_list:
                 attendance[self.get_year_string(batch)] = attendance.pop(batch)
