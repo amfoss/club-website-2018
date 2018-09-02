1d2a0d2d04feb8f21bd2d6e358d8bc09c368df76
dheerajmalisetty <malisettydheeraj@gmail.com>
2018-09-02 19:56:57 +0530

    Changes in aboutus page

---
+++ Home/static/home/assets/js/dheeraj.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    //Events that reset and restart the timer animation when the slides change
+    $("#transition-timer-carousel").on("slide.bs.carousel", function(event) {
+        //The animate class gets removed so that it jumps straight back to 0%
+        $(".transition-timer-carousel-progress-bar", this)
+            .removeClass("animate").css("width", "0%");
+    }).on("slid.bs.carousel", function(event) {
+        //The slide transition finished, so re-add the animate class so that
+        //the timer bar takes time to fill up
+        $(".transition-timer-carousel-progress-bar", this)
+            .addClass("animate").css("width", "100%");
+    });
+
+    //Kick off the initial slide animation when the document is ready
+    $(".transition-timer-carousel-progress-bar", "#transition-timer-carousel")
+        .css("width", "100%");
+});
