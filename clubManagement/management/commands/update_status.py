1c5994c086311a6c3654294e591f67b3953307f5
Chirath R <chirath.02@gmail.com>
2018-10-14 12:42:57 +0530

    Updated error log and create thread irrecspective of no status updates

--- clubManagement/management/commands/update_status.py
+++ clubManagement/management/commands/update_status.py
@@ -71,76 +71,93 @@ class Command(BaseCommand):
         email_list = get_status_update_emails(status_date)
         print(email_list)
 
-        if not email_list:
-            print("No email found, possible reasons: Google api Token might have expired.")
-            sys.exit()
-
-        # get all users in email_list
-        users = User.objects.all()
-        user_list = []
-
-        for user in users:
-            user_email = self.remove_periods_from_email(user.email)
-            if user_email in email_list:
-                user_list.append(user)
-
-        print(user_list)
-        # Generate string user_id Y, user_id Y etc....
-        status_string = ''
-
-        for user in user_list:
-            status_string += str(user.pk) + ' Y, '
-
-        status_string = status_string[:-2]
-
-        # create and process status update data
-        status_update, created = StatusUpdate.objects.get_or_create(
-            date=status_date)
-        status_update.value = status_string
-        status_update.process_report()
-
-        domain = settings.DOMAIN
-
-        url = 'https://%s%s' % (domain, reverse('status-update-detail',
-                                                kwargs={
-                                                    'day': status_date.day,
-                                                    'month': status_date.month,
-                                                    'year': status_date.year
-                                                }))
-
         mailing_list = settings.MAILING_LIST
         from_email = settings.EMAIL
 
-        if options.get('mail', False):
+        if email_list:
+            # get all users in email_list
+            users = User.objects.all()
+            user_list = []
 
-            if not mailing_list:
-                print('MAILING_LIST env variable needs to be set')
-                sys.exit()
+            for user in users:
+                user_email = self.remove_periods_from_email(user.email)
+                if user_email in email_list:
+                    user_list.append(user)
 
-            # Send status report
-            # render with dynamic value
-            html_content = render_to_string(
-                'clubManagement/status-report-mail-template.html', {
-                    'status_update': status_update.get_report(),
-                    'date': status_date,
-                    'url': url
-                })
+            print(user_list)
+            # Generate string user_id Y, user_id Y etc....
+            status_string = ''
 
-            # render text only mail
-            text_content = render_to_string(
-                'clubManagement/status-report-mail-template.txt', {
-                    'status_update': status_update.get_report(),
-                    'date': status_date,
-                    'url': url
-                })
+            for user in user_list:
+                status_string += str(user.pk) + ' Y, '
 
-            subject = 'Status update report %s' % status_date.strftime(
-                '%d-%m-%Y')
+            status_string = status_string[:-2]
 
-            email = EmailMultiAlternatives(
-                subject, text_content, from_email, [mailing_list])
-            email.attach_alternative(html_content, "text/html")
-            email.send()
+            # create and process status update data
+            status_update, created = StatusUpdate.objects.get_or_create(
+                date=status_date)
+            status_update.value = status_string
+            status_update.process_report()
+
+            domain = settings.DOMAIN
+
+            url = 'https://%s%s' % (domain, reverse('status-update-detail',
+                                                    kwargs={
+                                                        'day': status_date.day,
+                                                        'month': status_date.month,
+                                                        'year': status_date.year
+                                                    }))
+
+            mailing_list = settings.MAILING_LIST
+            from_email = settings.EMAIL
+
+            if options.get('mail', False):
+
+                if not mailing_list:
+                    print('MAILING_LIST env variable needs to be set')
+                    sys.exit()
+
+                # Send status report
+                # render with dynamic value
+                html_content = render_to_string(
+                    'clubManagement/status-report-mail-template.html', {
+                        'status_update': status_update.get_report(),
+                        'date': status_date,
+                        'url': url
+                    })
+
+                # render text only mail
+                text_content = render_to_string(
+                    'clubManagement/status-report-mail-template.txt', {
+                        'status_update': status_update.get_report(),
+                        'date': status_date,
+                        'url': url
+                    })
+
+                subject = 'Status update report %s' % status_date.strftime(
+                    '%d-%m-%Y')
+
+                email = EmailMultiAlternatives(
+                    subject, text_content, from_email, [mailing_list])
+                email.attach_alternative(html_content, "text/html")
+                email.send()
+
+            if options.get('telegram', False):
+                text_content = render_to_string(
+                    'clubManagement/status-report-telegram.txt', {
+                        'status_update': status_update.get_report(),
+                        'date': status_date,
+                        'url': url
+                    })
+
+                # Telegram bot
+                bot = telegram.Bot(
+                    token=settings.telegram_bot_token)
+                bot.send_message(
+                    chat_id=settings.telegram_group_id,
+                    text=text_content,
+                    parse_mode=telegram.ParseMode.HTML
+                )
 
         if options.get('create-thread', False):
             # Send next status update reminder
@@ -156,22 +173,4 @@ class Command(BaseCommand):
                 [mailing_list],
                 fail_silently=False,
             )
-
-        if options.get('telegram', False):
-            text_content = render_to_string(
-                'clubManagement/status-report-telegram.txt', {
-                    'status_update': status_update.get_report(),
-                    'date': status_date,
-                    'url': url
-                })
-
-            # Telegram bot
-            bot = telegram.Bot(
-                token=settings.telegram_bot_token)
-            bot.send_message(
-                chat_id=settings.telegram_group_id,
-                text=text_content,
-                parse_mode=telegram.ParseMode.HTML
-            )
-
         print("Status update processing successful")
